1f6ad659d25f76c7323bca230bb55a04afa89e3d
SteffenVBNet <36544308+SteffenVBNet@users.noreply.github.com>
2018-02-16 20:00:32 +0100

    Add files via upload

---
+++ Kran/Kran/Form1.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PanFundament = New System.Windows.Forms.Panel()
+        Me.PanSenkrechtesHauptelement = New System.Windows.Forms.Panel()
+        Me.PanAusleger = New System.Windows.Forms.Panel()
+        Me.PanHaken = New System.Windows.Forms.Panel()
+        Me.CmdHakenAus = New System.Windows.Forms.Button()
+        Me.CmdHakenEin = New System.Windows.Forms.Button()
+        Me.CmdAuslegerAus = New System.Windows.Forms.Button()
+        Me.CmdAuslegerEin = New System.Windows.Forms.Button()
+        Me.CmdKranRechts = New System.Windows.Forms.Button()
+        Me.TimHakenAus = New System.Windows.Forms.Timer(Me.components)
+        Me.CmdKranLinks = New System.Windows.Forms.Button()
+        Me.CmdKranHoch = New System.Windows.Forms.Button()
+        Me.CmdKranRunter = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'PanFundament
+        '
+        Me.PanFundament.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.PanFundament.Location = New System.Drawing.Point(512, 428)
+        Me.PanFundament.Name = "PanFundament"
+        Me.PanFundament.Size = New System.Drawing.Size(74, 24)
+        Me.PanFundament.TabIndex = 0
+        '
+        'PanSenkrechtesHauptelement
+        '
+        Me.PanSenkrechtesHauptelement.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.PanSenkrechtesHauptelement.Location = New System.Drawing.Point(539, 325)
+        Me.PanSenkrechtesHauptelement.Name = "PanSenkrechtesHauptelement"
+        Me.PanSenkrechtesHauptelement.Size = New System.Drawing.Size(23, 103)
+        Me.PanSenkrechtesHauptelement.TabIndex = 1
+        '
+        'PanAusleger
+        '
+        Me.PanAusleger.BackColor = System.Drawing.SystemColors.Desktop
+        Me.PanAusleger.Location = New System.Drawing.Point(458, 325)
+        Me.PanAusleger.Name = "PanAusleger"
+        Me.PanAusleger.Size = New System.Drawing.Size(81, 22)
+        Me.PanAusleger.TabIndex = 2
+        '
+        'PanHaken
+        '
+        Me.PanHaken.BackColor = System.Drawing.SystemColors.Highlight
+        Me.PanHaken.Location = New System.Drawing.Point(458, 347)
+        Me.PanHaken.Name = "PanHaken"
+        Me.PanHaken.Size = New System.Drawing.Size(21, 30)
+        Me.PanHaken.TabIndex = 0
+        '
+        'CmdHakenAus
+        '
+        Me.CmdHakenAus.Location = New System.Drawing.Point(1038, 100)
+        Me.CmdHakenAus.Name = "CmdHakenAus"
+        Me.CmdHakenAus.Size = New System.Drawing.Size(93, 23)
+        Me.CmdHakenAus.TabIndex = 3
+        Me.CmdHakenAus.Text = "Haken aus"
+        Me.CmdHakenAus.UseVisualStyleBackColor = True
+        '
+        'CmdHakenEin
+        '
+        Me.CmdHakenEin.Location = New System.Drawing.Point(1038, 129)
+        Me.CmdHakenEin.Name = "CmdHakenEin"
+        Me.CmdHakenEin.Size = New System.Drawing.Size(93, 23)
+        Me.CmdHakenEin.TabIndex = 4
+        Me.CmdHakenEin.Text = "Haken ein"
+        Me.CmdHakenEin.UseVisualStyleBackColor = True
+        '
+        'CmdAuslegerAus
+        '
+        Me.CmdAuslegerAus.Location = New System.Drawing.Point(1038, 158)
+        Me.CmdAuslegerAus.Name = "CmdAuslegerAus"
+        Me.CmdAuslegerAus.Size = New System.Drawing.Size(93, 23)
+        Me.CmdAuslegerAus.TabIndex = 5
+        Me.CmdAuslegerAus.Text = "Ausleger aus"
+        Me.CmdAuslegerAus.UseVisualStyleBackColor = True
+        '
+        'CmdAuslegerEin
+        '
+        Me.CmdAuslegerEin.Location = New System.Drawing.Point(1038, 187)
+        Me.CmdAuslegerEin.Name = "CmdAuslegerEin"
+        Me.CmdAuslegerEin.Size = New System.Drawing.Size(93, 23)
+        Me.CmdAuslegerEin.TabIndex = 6
+        Me.CmdAuslegerEin.Text = "Ausleger ein"
+        Me.CmdAuslegerEin.UseVisualStyleBackColor = True
+        '
+        'CmdKranRechts
+        '
+        Me.CmdKranRechts.Location = New System.Drawing.Point(1038, 216)
+        Me.CmdKranRechts.Name = "CmdKranRechts"
+        Me.CmdKranRechts.Size = New System.Drawing.Size(93, 23)
+        Me.CmdKranRechts.TabIndex = 7
+        Me.CmdKranRechts.Text = "Kran rechts"
+        Me.CmdKranRechts.UseVisualStyleBackColor = True
+        '
+        'TimHakenAus
+        '
+        '
+        'CmdKranLinks
+        '
+        Me.CmdKranLinks.Location = New System.Drawing.Point(1038, 245)
+        Me.CmdKranLinks.Name = "CmdKranLinks"
+        Me.CmdKranLinks.Size = New System.Drawing.Size(93, 23)
+        Me.CmdKranLinks.TabIndex = 8
+        Me.CmdKranLinks.Text = "Kran links"
+        Me.CmdKranLinks.UseVisualStyleBackColor = True
+        '
+        'CmdKranHoch
+        '
+        Me.CmdKranHoch.Location = New System.Drawing.Point(1038, 274)
+        Me.CmdKranHoch.Name = "CmdKranHoch"
+        Me.CmdKranHoch.Size = New System.Drawing.Size(93, 23)
+        Me.CmdKranHoch.TabIndex = 9
+        Me.CmdKranHoch.Text = "Kran Hoch"
+        Me.CmdKranHoch.UseVisualStyleBackColor = True
+        '
+        'CmdKranRunter
+        '
+        Me.CmdKranRunter.Location = New System.Drawing.Point(1038, 303)
+        Me.CmdKranRunter.Name = "CmdKranRunter"
+        Me.CmdKranRunter.Size = New System.Drawing.Size(93, 23)
+        Me.CmdKranRunter.TabIndex = 10
+        Me.CmdKranRunter.Text = "Kran Runter"
+        Me.CmdKranRunter.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1143, 635)
+        Me.Controls.Add(Me.CmdKranRunter)
+        Me.Controls.Add(Me.CmdKranHoch)
+        Me.Controls.Add(Me.CmdKranLinks)
+        Me.Controls.Add(Me.CmdKranRechts)
+        Me.Controls.Add(Me.CmdAuslegerEin)
+        Me.Controls.Add(Me.CmdAuslegerAus)
+        Me.Controls.Add(Me.CmdHakenEin)
+        Me.Controls.Add(Me.CmdHakenAus)
+        Me.Controls.Add(Me.PanHaken)
+        Me.Controls.Add(Me.PanAusleger)
+        Me.Controls.Add(Me.PanSenkrechtesHauptelement)
+        Me.Controls.Add(Me.PanFundament)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PanFundament As Panel
+    Friend WithEvents PanSenkrechtesHauptelement As Panel
+    Friend WithEvents PanAusleger As Panel
+    Friend WithEvents PanHaken As Panel
+    Friend WithEvents CmdHakenAus As Button
+    Friend WithEvents CmdHakenEin As Button
+    Friend WithEvents CmdAuslegerAus As Button
+    Friend WithEvents CmdAuslegerEin As Button
+    Friend WithEvents CmdKranRechts As Button
+    Friend WithEvents TimHakenAus As Timer
+    Friend WithEvents CmdKranLinks As Button
+    Friend WithEvents CmdKranHoch As Button
+    Friend WithEvents CmdKranRunter As Button
+End Class
---
+++ Kran/Kran/Form1.vb
@@ -0,0 +1,117 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        '    Me.Size = New Point(Me.Size.Width = 800, Me.Size.Height = 550)
+        CmdHakenAus.Location = New Point(100, 50)
+        CmdHakenEin.Location = New Point(100, 80)
+        CmdAuslegerAus.Location = New Point(100, 110)
+        CmdAuslegerEin.Location = New Point(100, 140)
+        CmdKranRechts.Location = New Point(100, 170)
+        CmdKranLinks.Location = New Point(100, 200)
+        'CmdHakenEin.Location = New Point(CmdHakenEin.Location.X, CmdHakenEin.Location.Y)
+        'CmdHakenAus.Location = New Point(CmdHakenAus.Location.X = 200, CmdHakenAus.Location.Y)
+    End Sub
+
+    Private Sub PanFunamen_Paint(sender As Object, e As PaintEventArgs) Handles PanFundament.Paint
+
+        PanFundament.BackColor = Color.Black
+        '   PanFundament.Location = New Point(512, 428)
+        '  PanFundament.Size = New Point(74, 24)
+        ' PanFundament.Location = New Point(PanFundament.Location.X = "200", PanFundament.Location.Y = "200")
+        '  PanFundament.Location = New Point(PanFundament.Location.X = 512, PanFundament.Location.Y = 428)
+        'PanFundament.Size = New Point(PanFundament.Size.Height = 24, PanFundament.Size.Width = 74)
+
+    End Sub
+
+    Private Sub PanHaken_Paint(sender As Object, e As PaintEventArgs) Handles PanHaken.Paint
+
+        PanHaken.BackColor = Color.Green
+        ' PanHaken.Location = New Point(458, 347)
+        ' PanHaken.Size = New Point(21, 30)
+
+    End Sub
+
+    Private Sub PanAusleger_Paint(sender As Object, e As PaintEventArgs) Handles PanAusleger.Paint
+
+        PanAusleger.BackColor = Color.Red
+        'PanAusleger.Location = New Point(458, 325)
+        'PanAusleger.Size = New Point(81, 22)
+
+
+        'PanAusleger.Location = New Point(PanAusleger.Location.X = 458, PanAusleger.Location.Y = 325)
+        ' PanAusleger.Size = New Point(PanAusleger.Size.Height = 22, PanAusleger.Size.Width = 81)
+
+    End Sub
+
+    Private Sub PanSenkrechtesHauptelement_Paint(sender As Object, e As PaintEventArgs) Handles PanSenkrechtesHauptelement.Paint
+
+        PanSenkrechtesHauptelement.BackColor = Color.Blue
+        '  PanSenkrechtesHauptelement.Location = New Point(539, 325)
+        ' PanSenkrechtesHauptelement.Size = New Point(23, 103)
+
+    End Sub
+
+    Private Sub CmdHakenEin_Click(sender As Object, e As EventArgs) Handles CmdHakenEin.Click
+
+        PanHaken.Height = PanHaken.Height - 10
+
+    End Sub
+
+    Private Sub CmdHakenAus_Click(sender As Object, e As EventArgs) Handles CmdHakenAus.Click
+
+        PanHaken.Height = PanHaken.Height + 10
+
+    End Sub
+
+    Private Sub CmdAuslegerAus_Click(sender As Object, e As EventArgs) Handles CmdAuslegerAus.Click
+
+        PanAusleger.Width = PanAusleger.Width + 10
+        PanAusleger.Location = New Point(PanAusleger.Location.X - 10, PanAusleger.Location.Y)
+        PanHaken.Location = New Point(PanHaken.Location.X - 10, PanHaken.Location.Y)
+
+    End Sub
+
+    Private Sub CmdAuslegerEin_Click(sender As Object, e As EventArgs) Handles CmdAuslegerEin.Click
+
+        PanAusleger.Width = PanAusleger.Width - 10
+        PanAusleger.Location = New Point(PanAusleger.Location.X + 10, PanAusleger.Location.Y)
+        PanHaken.Location = New Point(PanHaken.Location.X + 10, PanHaken.Location.Y)
+
+    End Sub
+
+    Private Sub CmdKranRechts_Click(sender As Object, e As EventArgs) Handles CmdKranRechts.Click
+        PanAusleger.Location = New Point(PanAusleger.Location.X + 10, PanAusleger.Location.Y)
+        PanFundament.Location = New Point(PanFundament.Location.X + 10, PanFundament.Location.Y)
+        PanHaken.Location = New Point(PanHaken.Location.X + 10, PanHaken.Location.Y)
+        PanSenkrechtesHauptelement.Location = New Point(PanSenkrechtesHauptelement.Location.X + 10, PanSenkrechtesHauptelement.Location.Y)
+    End Sub
+
+    Private Sub CmdKranLinks_Click(sender As Object, e As EventArgs) Handles CmdKranLinks.Click
+        PanAusleger.Location = New Point(PanAusleger.Location.X - 10, PanAusleger.Location.Y)
+        PanFundament.Location = New Point(PanFundament.Location.X - 10, PanFundament.Location.Y)
+        PanHaken.Location = New Point(PanHaken.Location.X - 10, PanHaken.Location.Y)
+        PanSenkrechtesHauptelement.Location = New Point(PanSenkrechtesHauptelement.Location.X - 10, PanSenkrechtesHauptelement.Location.Y)
+    End Sub
+
+    Private Sub TimHakenAus_Tick(sender As Object, e As EventArgs) Handles TimHakenAus.Tick
+        'PanHaken.Size = New Point(PanHaken.Size.Height + 20, PanHaken.Width)
+        PanHaken.Size = New Point(PanHaken.Size.Height + 10, PanHaken.Width)
+    End Sub
+
+    Private Sub CmdKranHoch_Click(sender As Object, e As EventArgs) Handles CmdKranHoch.Click
+
+        PanAusleger.Location = New Point(PanAusleger.Location.X, PanAusleger.Location.Y - 10)
+        PanFundament.Location = New Point(PanFundament.Location.X, PanFundament.Location.Y - 10)
+        PanHaken.Location = New Point(PanHaken.Location.X, PanHaken.Location.Y - 10)
+        PanSenkrechtesHauptelement.Location = New Point(PanSenkrechtesHauptelement.Location.X, PanSenkrechtesHauptelement.Location.Y - 10)
+
+    End Sub
+
+    Private Sub CmdKranRunter_Click(sender As Object, e As EventArgs) Handles CmdKranRunter.Click
+
+        PanAusleger.Location = New Point(PanAusleger.Location.X, PanAusleger.Location.Y + 10)
+        PanFundament.Location = New Point(PanFundament.Location.X, PanFundament.Location.Y + 10)
+        PanHaken.Location = New Point(PanHaken.Location.X, PanHaken.Location.Y + 10)
+        PanSenkrechtesHauptelement.Location = New Point(PanSenkrechtesHauptelement.Location.X, PanSenkrechtesHauptelement.Location.Y + 10)
+
+    End Sub
+End Class
